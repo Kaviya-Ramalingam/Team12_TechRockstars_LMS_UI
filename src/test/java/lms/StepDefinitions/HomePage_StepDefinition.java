a6affa5f72972216b96a135e93e2dfc2b00c3d13
kavitapakhale <97461496+kavitapakhale@users.noreply.github.com>
2025-02-20 12:43:51 -0500

    Home Page- feature and Step definition

---
+++ src/test/java/lms/StepDefinitions/HomePage_StepDefinition.java
@@ -0,0 +1,135 @@
+package lms.StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import lms.PageObjects.LoginPage;
+import lms.Utilities.ConfigReader;
+import lms.Utilities.LoggerLoad;
+import lms.Utilities.TestContextSetup;
+
+public class HomePage_StepDefinition {
+	
+	
+@Given("Admin is on login Page")
+public void admin_is_on_login_page() {
+    
+}
+
+@When("Admin enter valid data in all field and clicks login button")
+public void admin_enter_valid_data_in_all_field_and_clicks_login_button2() {
+       
+}
+
+@Then("Admin should see LMS -Learning management system as title")
+public void admin_should_see_lms_learning_management_system_as_title() {
+    
+}
+
+@Then("LMS title should be on the top left corner of page")
+public void lms_title_should_be_on_the_top_left_corner_of_page() {
+      
+}
+
+@Then("Admin should see correct spelling in navigation bar text")
+public void admin_should_see_correct_spelling_in_navigation_bar_text() {
+      
+}
+
+@When("Admin enter valid data in all field and  clicks login button")
+public void admin_enter_valid_data_in_all_field_and_clicks_login_button() {
+      
+}
+
+@Then("Admin should see correct spelling and space in LMS title")
+public void admin_should_see_correct_spelling_and_space_in_lms_title() {
+       
+}
+
+@Then("Admin should see the navigation bar text on the top right side")
+public void admin_should_see_the_navigation_bar_text_on_the_top_right_side() {
+    
+}
+
+@Then("Admin should see home in the 1st place")
+public void admin_should_see_home_in_the_1st_place() {
+     
+}
+
+@Then("Admin should see program in the 2nd place")
+public void admin_should_see_program_in_the_2nd_place() {
+      
+}
+
+@Then("Admin should see batch in the 3rd place")
+public void admin_should_see_batch_in_the_3rd_place() {
+       
+}
+
+@Then("Admin should see class in the 4th place")
+public void admin_should_see_class_in_the_4th_place() {
+       
+}
+
+@Then("Admin should see logout in the 5th place")
+public void admin_should_see_logout_in_the_5th_place() {
+       
+}
+
+
+@Then("Admin should see piechart")
+public void admin_should_see_piechart() {
+   
+}
+
+@Then("Admin should see welcome message with user name and role")
+public void admin_should_see_welcome_message_with_user_name_and_role() {
+    
+}
+
+@Then("Admin should see bar chart for Active and inactive user")
+public void admin_should_see_bar_chart_for_active_and_inactive_user() {
+    
+}
+
+@Then("Admin should see user count")
+public void admin_should_see_user_count() {
+    
+}
+
+@Then("Admin should see staff count")
+public void admin_should_see_staff_count() {
+  
+}
+
+@Then("Admin should see Program count")
+public void admin_should_see_program_count() {
+
+}
+
+@Then("Admin should see batch count")
+public void admin_should_see_batch_count() {
+   
+}
+
+@Then("Admin should see staff table with pagination icons")
+public void admin_should_see_staff_table_with_pagination_icons() {
+   
+}
+
+@Then("Admin should see {int} staff data in a page")
+public void admin_should_see_staff_data_in_a_page(Integer int1) {
+   
+}
+
+@Then("Admin should see previous page icon disabled")
+public void admin_should_see_previous_page_icon_disabled() {
+
+}
+
+@Then("Admin should see first page icon disabled")
+public void admin_should_see_first_page_icon_disabled() {
+   
+}
+
+}
