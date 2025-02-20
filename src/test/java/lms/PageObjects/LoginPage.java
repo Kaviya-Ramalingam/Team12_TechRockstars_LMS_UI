796bbea0cfeb45fd41a461a36dd8c03c6230ede3
Kaviya-Ramalingam <rkbevit@gmail.com>
2025-02-19 22:27:48 -0500

    loginPageObject

--- src/test/java/lms/PageObjects/LoginPage.java
+++ src/test/java/lms/PageObjects/LoginPage.java
@@ -7,40 +7,44 @@ import org.openqa.selenium.support.PageFactory;
 
 import lms.Utilities.CommonUtils;
 
-
 public class LoginPage {
-	
 
 	WebDriver driver;
 
-	public LoginPage(WebDriver driver)
-	{
+	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	@FindBy(id = "username")
 	WebElement username;
 	@FindBy(id = "password")
 	WebElement password;
 	@FindBy(id = "login")
 	WebElement login;
-	@FindBy(id ="mat-select-2")
+	@FindBy(xpath = "//div[@id='mat-select-value-1']/span")
 	WebElement role;
-	
-	
+	@FindBy(xpath = "//mat-option[1]/span[@class='mat-option-text']")
+	WebElement admin;
+
 	public void entervalidCredentials(String validUserName, String ValidPassword) {
-		CommonUtils.sendInput(driver,username, validUserName);
+		CommonUtils.sendInput(driver, username, validUserName);
 		CommonUtils.sendInput(driver, password, ValidPassword);
 	}
-	
-	public void selectRole(String VisibleText) {
-		CommonUtils.selectByVisibleText(driver, VisibleText, role);
+
+	public void selectRole() {
+		CommonUtils.moveToElementAndClick(driver, role);
+
 	}
 
 	public void loginClick() {
 		CommonUtils.moveToElementAndClick(driver, login);
+
 	}
 
+	public void selectAdmin(String VisibleText) {
+		CommonUtils.moveToElementAndClick(driver, admin);
+
+	}
 
 }
