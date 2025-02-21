12a1b854323ed08daf0447c8a32ebc141de5acad
Kaviya-Ramalingam <rkbevit@gmail.com>
2025-02-21 11:52:19 -0500

    pageobjectmanager

--- src/test/java/lms/PageObjects/PageObjectManager.java
+++ src/test/java/lms/PageObjects/PageObjectManager.java
@@ -3,18 +3,35 @@ package lms.PageObjects;
 import org.openqa.selenium.WebDriver;
 
 public class PageObjectManager {
-	
+
 	public LoginPage loginPage;
 	public WebDriver driver;
-	
-	
+	public ProgramPage programPage;
+	public BatchPage bp;
+	public ClassPage classPage;
+
 	public PageObjectManager(WebDriver driver) {
-		this.driver=driver;
+		this.driver = driver;
 	}
 
 	public LoginPage getLoginPage() {
-		 loginPage = new LoginPage(driver);
-		 return loginPage;
+		loginPage = new LoginPage(driver);
+		return loginPage;
+	}
+
+	public ProgramPage getProgramPage() {
+		programPage = new ProgramPage(driver);
+		return programPage;
+	}
+
+	public BatchPage getBatchPage() {
+		bp = new BatchPage(driver);
+		return bp;
+	}
+
+	public ClassPage getClassPage() {
+		classPage = new ClassPage(driver);
+		return classPage;
 	}
 
 }
