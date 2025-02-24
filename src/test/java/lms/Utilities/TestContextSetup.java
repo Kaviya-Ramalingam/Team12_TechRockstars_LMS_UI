22626a218386ba927d37778a35b78a321764daf3
Madhuchhanda Sahu <madhuchhanda.sahu03@gmail.com>
2025-02-23 22:57:19 -0500

    testcon

--- src/test/java/lms/Utilities/TestContextSetup.java
+++ src/test/java/lms/Utilities/TestContextSetup.java
@@ -1,5 +1,7 @@
 package lms.Utilities;
 
+import java.io.IOException;
+
 import lms.DriverFactory.DriverFactory;
 import lms.PageObjects.PageObjectManager;
 
@@ -8,12 +10,13 @@ public class TestContextSetup {
 	public DriverFactory driverfactory;
 
 	public PageObjectManager pageobjectmanager;
-	public CommonUtils commonutils;
-
+	//public CommonUtils commonutils;
+	//public ConfigReader configReader;
 	public TestContextSetup() {
 		driverfactory = new DriverFactory();
 		pageobjectmanager = new PageObjectManager(DriverFactory.getdriver());
-		commonutils = new CommonUtils(DriverFactory.getdriver());
+		//commonutils = new CommonUtils(DriverFactory.getdriver());
+		//configReader=new ConfigReader();
 	}
 
 
